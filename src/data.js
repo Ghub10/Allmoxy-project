a0694a1b3dfbb912416cab20a0bf459065eda5e6
alejandro abonnanzieri <alejandroabonnanzieri@alejandros-MacBook-Pro.local>
2021-07-15 11:54:03 -0600

    updated changes

--- src/data.js
+++ src/data.js
@@ -1,4 +1,3 @@
-
 import faker from 'faker';
 
 const products = [
@@ -10,18 +9,18 @@ const products = [
     image: "https://images.unsplash.com/photo-1592921870789-04563d55041c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
   },
   {
-    title: 'iPhone',
-    description: faker.commerce.productDescription(),
+    title: 'iphone',
+    description: "most awsome phones out there",
     price: faker.commerce.price(),
     qty: Math.floor(Math.random() * 11),
-    image: faker.image.business()
+    image: "https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
   },
   {
     title: 'iPad',
     description: faker.commerce.productDescription(),
     price: faker.commerce.price(),
     qty: Math.floor(Math.random() * 11),
-    image: faker.image.business()
+    image: "https://images.unsplash.com/photo-1557825835-70d97c4aa567?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
   },
   {
     title: 'Macbook Pro',
